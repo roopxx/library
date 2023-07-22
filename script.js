5480a8e6f291d3d668685f5662076ef243cb1ca3
calme-roops <127219908+calme-roops@users.noreply.github.com>
2023-07-22 18:36:35 +0530

    Add Toggle & Remove Buttons, Handle Duplicates
    In this commit, I made improvements to the displayBook() function to
    display books in the library more efficiently. I replaced the previous
    for loop with a more concise one, and now each book is appended to the
    DOM as it is iterated through, reducing unnecessary repetitions.
    
    Additionally, I added the functionality for the "Read/Not Read" toggle
    button and the "Remove" button in the displayBook() function.

--- script.js
+++ script.js
@@ -15,55 +15,97 @@ function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus) {
   myLibrary.push(newBook);
 }
 
+// Function to display a single book in the library
+function displayBook(book) {
+  var div = document.createElement("div");
+  div.classList.add("book-sample");
+  var title = document.createElement("p");
+  title.classList.add("title");
+  var author = document.createElement("p");
+  author.classList.add("author");
+  var pages = document.createElement("span");
+  pages.classList.add("pages");
+  var status = document.createElement("button");
+  status.classList.add("status");
+  var removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-book");
+
+  div.append(title);
+  div.append(author);
+  div.append(pages);
+  div.append(status);
+  div.append(removeBtn);
+
+  title.innerText = book.title;
+  author.innerText = book.author;
+  pages.innerText = book.pages + " pages";
+  status.innerText = book.status;
+
+  // Add event listener for status button
+  status.addEventListener("click", () => {
+    book.status = book.status === "Read" ? "Not read" : "Read";
+    displayBooks();
+  });
+
+  // Toggle color based on status
+  status.classList.toggle("btn-light-green", book.status === "Read");
+  status.classList.toggle("btn-light-red", book.status === "Not read");
+
+  // Add event listener for remove button
+  removeBtn.addEventListener("click", () => {
+    removeBookByTitle(book.title);
+  });
+
+  return div;
+}
+
 // Function to display books in the library
-function displayBook() {
-  for (var book of myLibrary) {
-    var view = document.querySelector(".flexview");
-    var div = document.createElement("div");
-    div.classList.add("book-sample");
-    var title = document.createElement("p");
-    title.classList.add("title");
-    var author = document.createElement("p");
-    author.classList.add("author");
-    var pages = document.createElement("span");
-    pages.classList.add("pages");
-    var status = document.createElement("button");
-    status.classList.add("status");
-    var removeBtn = document.createElement("button");
-    removeBtn.classList.add("remove-book");
-    removeBtn.textContent = "Remove";
+function displayBooks() {
+  const view = document.querySelector(".flexview");
+  view.innerHTML = "";
 
-    div.append(title);
-    div.append(author);
-    div.append(pages);
-    div.append(status);
-    div.append(removeBtn);
-    view.append(div);
+  const fragment = document.createDocumentFragment();
 
-    title.innerText = book.title;
-    author.innerText = book.author;
-    pages.innerText = book.pages;
-    status.innerText = book.status;
+  for (const book of myLibrary) {
+    const bookElement = displayBook(book);
+    fragment.appendChild(bookElement);
   }
+
+  view.appendChild(fragment);
+}
+
+function clearForm() {
+  document.querySelector('input[id="title"]').value = null;
+  document.querySelector('input[id="author"]').value = null;
+  document.querySelector('input[id="pages"]').value = null;
+  document.querySelector('input[id="pages"]').value = null;
+  document.querySelector('input[id="status"]').checked = false;
+}
+
+// Function to remove a book by its title
+function removeBookByTitle(bookTitle) {
+  myLibrary = myLibrary.filter((book) => book.title !== bookTitle);
+  displayBooks();
 }
 
 // Variables declaration
-var addBookBtn = document.querySelector(".add-book");
-var resetLibraryBtn = document.querySelector(".reset-library");
-var addBtn = document.querySelector(".add-button");
-var formDisplay = document.querySelector(".overlay");
-var container = document.querySelector(".container");
+const addBookBtn = document.querySelector(".add-book");
+const resetLibraryBtn = document.querySelector(".reset-library");
+const addBtn = document.querySelector(".add-button");
+const formDisplay = document.querySelector(".overlay");
+const bookSection = document.querySelector(".display-book");
 
 // Event Listeners
 addBookBtn.addEventListener("click", () => {
   formDisplay.classList.toggle("formDisplay");
-  container.classList.toggle("blur");
+  bookSection.classList.toggle("blur");
 });
 
 resetLibraryBtn.addEventListener("click", () => {
-  var bookDisplay = document.querySelector(".display-book");
-  confirm("Do you want to delete all your books data?");
-  bookDisplay.style.display = "none";
+  if (confirm("Do you want to delete all your books data?")) {
+    myLibrary = [];
+    displayBooks();
+  }
 });
 
 addBtn.addEventListener("click", (event) => {
@@ -79,8 +121,9 @@ addBtn.addEventListener("click", (event) => {
     alert("Book already exists!!");
   } else {
     addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus);
-    displayBook();
+    displayBooks();
   }
   formDisplay.classList.toggle("formDisplay");
-  container.classList.toggle("blur");
+  bookSection.classList.toggle("blur");
+  clearForm();
 });
