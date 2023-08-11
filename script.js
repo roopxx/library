bd9659094e3dafd1cc7d2af8b3bb606f42328bed
roopxx <127219908+roopxx@users.noreply.github.com>
2023-08-11 14:27:19 +0530

    Revamp with ES6 classes, enhance library features
    - Migrate book-related functionalities into a Book class
    - Create a Library class to manage the book collection
    - Update event listeners to work with new class structure
    - Maintain existing UI features and interactions

--- script.js
+++ script.js
@@ -1,78 +1,135 @@
-// Data array
-let myLibrary = [];
-
-// Object to hold book data
-function Book(title, author, pages, status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
+//Class declaration for book
+class Book {
+  constructor(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
 }
 
-// Function to add new book to array myLibrary
-function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus) {
-  var newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
-  myLibrary.push(newBook);
-}
-
-// Function to display a single book in the library
-function displayBook(book) {
-  var div = document.createElement("div");
-  div.classList.add("book-sample");
-  var title = document.createElement("p");
-  title.classList.add("title");
-  var author = document.createElement("p");
-  author.classList.add("author");
-  var pages = document.createElement("span");
-  pages.classList.add("pages");
-  var status = document.createElement("button");
-  status.classList.add("status");
-  var removeBtn = document.createElement("button");
-  removeBtn.classList.add("remove-book");
-
-  div.append(title);
-  div.append(author);
-  div.append(pages);
-  div.append(status);
-  div.append(removeBtn);
-
-  title.innerText = book.title;
-  author.innerText = book.author;
-  pages.innerText = book.pages + " pages";
-  status.innerText = book.status;
-
-  // Add event listener for status button
-  status.addEventListener("click", () => {
-    book.status = book.status === "Read" ? "Not read" : "Read";
-    displayBooks();
-  });
-
-  // Toggle color based on status
-  status.classList.toggle("btn-light-green", book.status === "Read");
-  status.classList.toggle("btn-light-red", book.status === "Not read");
-
-  // Add event listener for remove button
-  removeBtn.addEventListener("click", () => {
-    removeBookByTitle(book.title);
-  });
-
-  return div;
-}
-
-// Function to display books in the library
-function displayBooks() {
-  const view = document.querySelector(".flexview");
-  view.innerHTML = "";
-
-  const fragment = document.createDocumentFragment();
-
-  for (const book of myLibrary) {
-    const bookElement = displayBook(book);
-    fragment.appendChild(bookElement);
+//Class declaration for library with its methods
+class Library {
+  constructor() {
+    this.myLibrary = [];
   }
 
-  view.appendChild(fragment);
+  // Method to add a book to the library
+  addBookToLibrary(book) {
+    this.myLibrary.push(book);
+  }
+
+  // Method to remove a book from the library by title
+  removeBookByTitle(bookTitle) {
+    this.myLibrary = this.myLibrary.filter((book) => book.title !== bookTitle);
+  }
+
+  // Method to toggle the status (Read/Not read) of a book
+  toggleBookStatus(bookTitle) {
+    const book = this.myLibrary.find((book) => book.title === bookTitle);
+    if (book) {
+      book.status = book.status === "Read" ? "Not read" : "Read";
+    }
+  }
+
+  // Method to display all books in the library
+  displayBooks() {
+    const view = document.querySelector(".flexview");
+    view.innerHTML = "";
+
+    const fragment = document.createDocumentFragment();
+
+    for (const book of this.myLibrary) {
+      const bookElement = this.displayBook(book);
+      fragment.appendChild(bookElement);
+    }
+
+    view.appendChild(fragment);
+  }
+
+  // Method to create the display for a single book
+  displayBook(book) {
+    const div = document.createElement("div");
+    div.classList.add("book-sample");
+
+    const title = document.createElement("p");
+    title.classList.add("title");
+    const author = document.createElement("p");
+    author.classList.add("author");
+    const pages = document.createElement("span");
+    pages.classList.add("pages");
+    const status = document.createElement("button");
+    status.classList.add("status");
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-book");
+
+    div.append(title);
+    div.append(author);
+    div.append(pages);
+    div.append(status);
+    div.append(removeBtn);
+
+    title.innerText = book.title;
+    author.innerText = book.author;
+    pages.innerText = book.pages + " pages";
+    status.innerText = book.status;
+
+    status.addEventListener("click", () => {
+      this.toggleBookStatus(book.title);
+      this.displayBooks();
+    });
+
+    status.classList.toggle("btn-light-green", book.status === "Read");
+    status.classList.toggle("btn-light-red", book.status === "Not read");
+
+    removeBtn.addEventListener("click", () => {
+      this.removeBookByTitle(book.title);
+      this.displayBooks();
+    });
+
+    return div;
+  }
 }
+
+const library = new Library();
+
+const addBookBtn = document.querySelector(".add-book");
+const resetLibraryBtn = document.querySelector(".reset-library");
+const addBtn = document.querySelector(".add-button");
+const formDisplay = document.querySelector(".overlay");
+const bookSection = document.querySelector(".display-book");
+
+addBookBtn.addEventListener("click", () => {
+  formDisplay.classList.toggle("formDisplay");
+  bookSection.classList.toggle("blur");
+});
+
+resetLibraryBtn.addEventListener("click", () => {
+  if (confirm("Do you want to delete all your books data?")) {
+    library.myLibrary = [];
+    library.displayBooks();
+  }
+});
+
+addBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const bookTitle = document.querySelector('input[id="title"]').value;
+  const bookAuthor = document.querySelector('input[id="author"]').value;
+  const bookPages = document.querySelector('input[id="pages"]').value;
+  const bookStatus = document.querySelector('input[id="status"]:checked').value;
+
+  const bookExists = library.myLibrary.some((book) => book.title === bookTitle);
+  if (bookExists) {
+    alert("Book already exists!!");
+  } else {
+    const newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+    library.addBookToLibrary(newBook);
+    library.displayBooks();
+  }
+  formDisplay.classList.toggle("formDisplay");
+  bookSection.classList.toggle("blur");
+  clearForm();
+});
 
 function clearForm() {
   document.querySelector('input[id="title"]').value = null;
@@ -81,49 +138,3 @@ function clearForm() {
   document.querySelector('input[id="pages"]').value = null;
   document.querySelector('input[id="status"]').checked = false;
 }
-
-// Function to remove a book by its title
-function removeBookByTitle(bookTitle) {
-  myLibrary = myLibrary.filter((book) => book.title !== bookTitle);
-  displayBooks();
-}
-
-// Variables declaration
-const addBookBtn = document.querySelector(".add-book");
-const resetLibraryBtn = document.querySelector(".reset-library");
-const addBtn = document.querySelector(".add-button");
-const formDisplay = document.querySelector(".overlay");
-const bookSection = document.querySelector(".display-book");
-
-// Event Listeners
-addBookBtn.addEventListener("click", () => {
-  formDisplay.classList.toggle("formDisplay");
-  bookSection.classList.toggle("blur");
-});
-
-resetLibraryBtn.addEventListener("click", () => {
-  if (confirm("Do you want to delete all your books data?")) {
-    myLibrary = [];
-    displayBooks();
-  }
-});
-
-addBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  var bookTitle = document.querySelector('input[id="title"]').value;
-  var bookAuthor = document.querySelector('input[id="author"]').value;
-  var bookPages = document.querySelector('input[id="pages"]').value;
-  var bookStatus = document.querySelector('input[id="status"]:checked').value;
-
-  // To check whether the book already is on display
-  var bookExists = myLibrary.some((book) => book.title === bookTitle);
-  if (bookExists) {
-    alert("Book already exists!!");
-  } else {
-    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus);
-    displayBooks();
-  }
-  formDisplay.classList.toggle("formDisplay");
-  bookSection.classList.toggle("blur");
-  clearForm();
-});
